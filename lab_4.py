2f6ce63a012d13f4713f1db141a63704fc11ec70
garand277 <145905657+garand277@users.noreply.github.com>
2024-03-22 22:03:28 +0500

    Add files via upload

---
+++ lab_4.py
@@ -0,0 +1,45 @@
+import requests, ast
+
+def get_response():
+    response = 0
+    def get_body():
+        nonlocal response
+        response = requests.get('https://dogapi.dog/api/v2/facts')
+        dictionary = ast.literal_eval(response.text)
+        body = dictionary['data'][0]['attributes']['body']
+        print(body)
+    return get_body
+
+dog_facts = get_response()
+dog_facts()
+dog_facts()
+dog_facts()
+dog_facts()
+dog_facts()
+
+
+
+def call_lim(func):
+    t = 5
+    def wrapper():
+        nonlocal t
+        if t > 0:
+            print(f'/Осталось вызовов: {t - 1}/')
+            func()
+        else:
+            print('/Вызов функции заблокирован/')
+        t -= 1
+    return wrapper
+
+@call_lim
+def hello():
+    print('Hello')
+
+hello()
+hello()
+hello()
+hello()
+hello()
+hello()
+hello()
+hello()
